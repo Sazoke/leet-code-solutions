215b4c19d84c6cf09b9acc3efeb3054a59409d1f
Solovev Ivan <sazokuto@gmail.com>
2021-08-10 14:14:12 +0500

    Add Solution

--- Solution/Solution.cs
+++ Solution/Solution.cs
@@ -1,7 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Solution
 {
     public class Solution
     {
-        
+        public int MinFlipsMonoIncr(string s)
+        {
+            var oneCount = 0;
+            var flipCount = 0;
+            foreach (var number in s)
+            {
+                if (number == '0')
+                {
+                    if(oneCount == 0)
+                        continue;
+                    flipCount++;
+                }
+                else
+                    oneCount++;
+
+                if (oneCount < flipCount)
+                    flipCount = oneCount;
+            }
+
+            return flipCount;
+        }
     }
 }
