75be6c8a8c71d6a38b9682eb7a18c45053ddd657
Solovev Ivan <sazokuto@gmail.com>
2021-08-12 17:24:18 +0500

    Add realization

--- Solution/Solution.cs
+++ Solution/Solution.cs
@@ -1,7 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Solution
 {
     public class Solution
     {
-        
+        public List<string>[] GroupAnagrams(string[] strs)
+        {
+            var result = new List<List<string>>();
+            var dict = new Dictionary<string, int>();
+            foreach (var str in strs)
+            {
+                var copy = new string(str.OrderBy(c => c).ToArray());
+                if (!dict.ContainsKey(copy))
+                {
+                    dict[copy] = result.Count;
+                    result.Add(new List<string>() {str});
+                }
+                else
+                    result[dict[copy]].Add(str);
+            }
+
+            return result.ToArray();
+        }
     }
 }
